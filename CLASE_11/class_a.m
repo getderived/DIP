3294f3063bfa9d0d3e4ce257e77fb71e67e6698d
jadope <jeronimoal10@gmail.com>
2018-04-13 10:48:52 -0500

    Class 11 and 10

--- CLASE_11/class_a.m
+++ CLASE_11/class_a.m
@@ -3,3 +3,5 @@ a=imread('084.jpg');
 [a1,a2,a3,a4,a5]= componentes(a);
 figure(1);imshow([a1;a2;a3;a4;a5]); impixelinfo
 title(['rgb -hsv - cmy - kkk - lab']) %Se observa que la b de rgb, s, y y la b1 de lab, determinan el lunor melo.
+s=componente(a2, 2);
+b=componente(a1),3
